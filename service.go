8515600ccdb587b4271367c1598fe4c71406ee83
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2023-06-27 18:45:00 +0200

    adjust service impl

--- service.go
+++ service.go
@@ -1,7 +1,11 @@
 package filestore
 
 import (
+	"bytes"
 	"context"
+	"crypto"
+	"errors"
+	"io"
 
 	"google.golang.org/grpc"
 
@@ -9,19 +13,76 @@ import (
 )
 
 type service struct {
-	pb.UnsafeFileServer
+	pb.UnimplementedFileServer
+
+	rootDir string
 }
 
-func NewService() service {
-	return service{}
+func NewService(rootDir string) service {
+	return service{
+		rootDir: rootDir,
+	}
 }
 
 func (s service) ListFiles(context.Context, *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
-	return nil, nil
+	paths, err := listAllFiles(s.rootDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ListFilesResponse{
+		Paths: paths,
+	}, nil
 }
 
 func (s service) StoreFile(stream pb.File_StoreFileServer) error {
-	return nil
+	rec, err := stream.Recv()
+	if err != nil {
+
+	}
+
+	path := rec.GetPath()
+	if path == "" {
+
+	}
+
+	errCh := make(chan error)
+	dataCh := make(chan []byte)
+	go func() {
+		err := saveFile(s.rootDir, path, dataCh)
+		if err != nil {
+			errCh <- err
+		}
+	}()
+	md5 := crypto.MD5.New()
+	md5.Write(rec.GetChunkData())
+	dataCh <- rec.GetChunkData()
+
+	receivedMD5 := rec.GetMd_5()
+	for {
+		rec, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+		}
+		md5.Write(rec.GetChunkData())
+		dataCh <- rec.GetChunkData()
+
+		if len(rec.Md_5) > 0 {
+			receivedMD5 = rec.GetMd_5()
+		}
+	}
+	close(dataCh)
+
+	if err := <-errCh; err != nil {
+		return err
+	}
+	if !bytes.Equal(receivedMD5, md5.Sum(nil)) {
+		return errors.New("invalid md5")
+	}
+
+	return stream.SendAndClose(&pb.StoreFileResponse{})
 }
 
 func (s service) RegisterIn(serv *grpc.Server) {
